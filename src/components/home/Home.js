63bbda9cc8a703a54e1cd58081b76e669ab6bde4
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-09-15 21:19:34 +0200

    add hotels to home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { BASE_URL, headers } from "../../constants/api";
 
 function Home() {
-    return <h1>Home page</h1>;
+    const [hotels, setHotels] = useState([]);
+
+    const url = BASE_URL + "establishments";
+
+    const options = { headers };
+
+
+    useEffect(() => {
+        fetch(url, options)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json);
+                setHotels(json);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <>
+             {hotels.map((hotel) => {
+                    return (
+                        <li key={hotel.id}>
+                            <NavLink to={`/admin/hotels/edit/${hotel.id}`}>{hotel.name}</NavLink>
+                        </li>
+                    );
+                })}
+        </>
+    )
 }
 
 export default Home;
+
+
+
+ 
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,14 @@
-const KEY = "your-key";
-export const BASE_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/v1/";
+const KEY = "5f57e328fe6b09bde55f9d2c";
+const url = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/v1/";
+const proxy = "https://noroffcors.herokuapp.com/";
+export const BASE_URL = proxy + url;
+
+
 export const headers = {
     "Content-Type": "application/json",
     key: KEY,
 };
 export const PATCH = "PATCH";
 export const DELETE = "DELETE";
+
+//const establishmentsUrl = BASE_URL + "establishments";
