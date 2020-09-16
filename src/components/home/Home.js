19623778be3fc0c45dd654aec1480f18064614b5
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-09-16 11:16:16 +0200

    add custom pages

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { NavLink } from "react-router-dom";
 import { BASE_URL, headers } from "../../constants/api";
 
 function Home() {
     const [hotels, setHotels] = useState([]);
-
+    const { register, handleSubmit } = useForm();
+    const history = useHistory();
+    const data = "";
     const url = BASE_URL + "establishments";
-
     const options = { headers };
+
+  
+    async function onSubmit(data) {
+        console.log("data", data);
+        const url = BASE_URL + "contacts";
+        const options = { headers, method: "POST", body: JSON.stringify(data) };
+    
+        await fetch(url, options);
+        history.push("/admin/hotels")
+    }
+   
 
 
     useEffect(() => {
@@ -22,6 +38,7 @@ function Home() {
 
     return (
         <>
+        <h1>Hotels</h1>
              {hotels.map((hotel) => {
                     return (
                         <li key={hotel.id}>
@@ -29,6 +46,30 @@ function Home() {
                         </li>
                     );
                 })}
+
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <h2>Questions?</h2>
+            <Form.Group>
+                <Form.Label>
+                    Name
+                </Form.Label>
+                <Form.Control name="name" placeholder="Enter your name" ref={register} />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>
+                    E-mail
+                </Form.Label>
+                <Form.Control name="email" placeholder="Enter your e-mail address" ref={register} />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>
+                    Message
+                </Form.Label>
+                <Form.Control name="email" placeholder="Enter your e-mail address" ref={register} />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form>
         </>
     )
 }
