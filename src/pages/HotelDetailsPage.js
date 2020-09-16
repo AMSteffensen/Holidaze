f7a8b6cbd321319f85342cf7390264326644ee9e
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-09-16 12:32:55 +0200

    add routes for pages

---
+++ src/pages/HotelDetailsPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const HotelDetailPage = () => (
+    <>
+        <h1>Hotel Details page</h1>
+        <p>Welcome to our site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae autem iste fugit. Soluta, molestias aspernatur consequuntur esse rerum explicabo! Quisquam odit placeat fugit delectus iusto!</p>
+    </>
+)  
+
+export default HotelDetailPage;
+
---
+++ src/pages/HotelPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const HotelPage = () => (
+    <>
+        <h1>This is a Hotel Title</h1>
+        <p>This is some hotel description text. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, eveniet.</p>
+    </>
+)  
+
+export default HotelPage;
+
