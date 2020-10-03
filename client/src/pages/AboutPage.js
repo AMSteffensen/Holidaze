c22bc51f269bed8520fb5aa0a94c775b6488f1ed
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-10-03 13:13:12 +0200

    display search results on input click

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-
+import Container from 'react-bootstrap/Container';
 
 const AboutPage = () => (
     <>
-        <h1>About us</h1>
-        <p>Welcome to our site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae autem iste fugit. Soluta, molestias aspernatur consequuntur esse rerum explicabo! Quisquam odit placeat fugit delectus iusto!</p>
+        <Container>
+            <h1>About us</h1>
+            <p>Welcome to our site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae autem iste fugit. Soluta, molestias aspernatur consequuntur esse rerum explicabo! Quisquam odit placeat fugit delectus iusto!</p>
+        </Container>
     </>
 )  
 
--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -34,16 +34,18 @@ function ContactPage() {
     }
 
     return (
-        <Container>
-            <h1>Contact us</h1>
-            <p>Welcome to our site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae autem iste fugit. Soluta, molestias aspernatur consequuntur esse rerum explicabo! Quisquam odit placeat fugit delectus iusto!</p>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Input name="firstName" placeholder="First name" ref={register({ required: true })}/>
+        <>
+            <Container>
+                    <h1>Contact us</h1>
+                    <p>Welcome to our site. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae autem iste fugit. Soluta, molestias aspernatur consequuntur esse rerum explicabo! Quisquam odit placeat fugit delectus iusto!</p>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Input name="firstName" placeholder="First name" ref={register({ required: true })}/>
 
-                <Button/>
-            </Form>
-        
-    </Container>
+                        <Button/>
+                    </Form>
+                
+            </Container>
+        </>
     )
     
 }
