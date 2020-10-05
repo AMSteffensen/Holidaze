e2e07e4439b9f3873f05a02f5575245898a54a01
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-10-05 11:29:10 +0200

    change react autocomplete with bootstrap

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,8 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, {Fragment, useState, useEffect} from "react";
 import {BASE_URL, headers} from "../../constants/api";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Autocomplete from "react-autocomplete";
+import {Form} from "react-bootstrap";
+import {Typeahead} from "react-bootstrap-typeahead";
 
 function Search() {
   const [accommodations, setAccommodations] = useState([]);
@@ -11,6 +13,9 @@ function Search() {
   const establishmentURL = BASE_URL + "establishments";
 
   const options = {headers};
+
+  const [singleSelections, setSingleSelections] = useState([]);
+  const [multiSelections, setMultiSelections] = useState([]);
 
   useEffect(() => {
     fetch(establishmentURL, options)
@@ -29,33 +34,20 @@ function Search() {
   return (
     <>
       <Container>
-        <Autocomplete
-          items={accommodations}
-          shouldItemRender={(accommodations, value) =>
-            accommodations.name.toLowerCase().indexOf(value.toLowerCase()) > -1
-          }
-          getItemValue={(accommodations) => accommodations.name}
-          menuStyle={{
-            borderRadius: "3px",
-            boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
-            background: "rgba(255, 255, 255, 0.9)",
-            padding: "5px",
-            fontSize: "90%",
-            position: "fixed",
-            overflow: "auto",
-            maxHeight: "50%",
-            width: "50%",
-          }}
-          
-          renderItem={(accommodations) => (
-            <Col md={12} key={accommodations.id}>
-              {accommodations.name}
-            </Col>
-          )}
-          value={accommodations.name}
-          onChange={(e) => (accommodations.name = e.target.value)}
-          onSelect={(id) => (console.log(id)) }
-        />
+        <Fragment>
+          <Form.Group>
+            <Form.Label>Search accommodations</Form.Label>
+            <Typeahead
+              id='basic-typeahead-single'
+              labelKey='name'
+              onChange={setSingleSelections}
+              options={accommodations}
+              placeholder='Choose a state...'
+              selected={singleSelections}
+              onClick={() => console.log('click!')}
+            />
+          </Form.Group>
+        </Fragment>
       </Container>
     </>
   );
