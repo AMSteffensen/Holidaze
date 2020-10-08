963b2eadba6845d8e563675d149e431743b55558
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-10-08 11:27:02 +0200

    move fetch accomodation to home component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,27 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
-import { BASE_URL } from "../../constants/api";
+import {BASE_URL, headers} from "../../constants/api";
 
 // import components
 import Search from "../search/Search";
 
 const Container = styled.div`
-    max-width: 1100px;
-    margin: 0 auto;
-    padding: 2em;
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 2em;
 `;
 
 function Home() {
+  const [accomodations, setAccommodations] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <Container>
-            <Search /> 
-        </Container>
-    )
+  useEffect(() => {
+    const establishmentURL = BASE_URL + "establishments";
+    const options = {headers};
+    fetch(establishmentURL, options)
+      .then((response) => response.json())
+      .then((json) => {
+        setAccommodations(json);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  
+  return (
+    <>
+      <Search />
+    </>
+  );
 }
 
 export default Home;
-
-
-
- 
--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,54 +1,19 @@
 import React, {Fragment, useState, useEffect} from "react";
-import {BASE_URL, headers} from "../../constants/api";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Autocomplete from "react-autocomplete";
-import {Form} from "react-bootstrap";
-import {Typeahead} from "react-bootstrap-typeahead";
 
-function Search() {
-  const [accommodations, setAccommodations] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const establishmentURL = BASE_URL + "establishments";
-
-  const options = {headers};
-
+function Search({accomodations}) {
   const [singleSelections, setSingleSelections] = useState([]);
   const [multiSelections, setMultiSelections] = useState([]);
 
-  useEffect(() => {
-    fetch(establishmentURL, options)
-      .then((response) => response.json())
-      .then((json) => {
-        setAccommodations(json);
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }, [options, establishmentURL]);
-
-  if (loading) {
-    return <div>...Loading</div>;
-  }
-
   return (
     <>
-      <Container>
-        <Fragment>
-          <Form.Group>
-            <Form.Label>Search accommodations</Form.Label>
-            <Typeahead
-              id='basic-typeahead-single'
-              labelKey='name'
-              onChange={setSingleSelections}
-              options={accommodations}
-              placeholder='Choose a state...'
-              selected={singleSelections}
-              onClick={() => console.log('click!')}
-            />
-          </Form.Group>
-        </Fragment>
-      </Container>
+      <div className='p-4'>
+        <form>
+          <input
+            className='p-2 rounded-bl-md w-full shadow-lg outline-none'
+            placeholder='Search hotels'
+          ></input>
+        </form>
+      </div>
     </>
   );
 }
