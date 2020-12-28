6a561163ad5f32907a0a07c8727db03b1548183c
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-12-28 17:41:30 +0100

    Updates

---
+++ precache-manifest.5789281b4296f39a15d10fb41fea0f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a336c9fe91a648ff5206e80464f2f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96ec3cdf3fa52935d13",
+    "url": "/static/css/2.24daf3d1.chunk.css"
+  },
+  {
+    "revision": "a4270960cf793f3c49fb",
+    "url": "/static/css/main.5fd446c1.chunk.css"
+  },
+  {
+    "revision": "b96ec3cdf3fa52935d13",
+    "url": "/static/js/2.d4d1a4bf.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.d4d1a4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4270960cf793f3c49fb",
+    "url": "/static/js/main.4379e6d1.chunk.js"
+  },
+  {
+    "revision": "cba04bb5ec3d51678e68",
+    "url": "/static/js/runtime-main.32c7cc17.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/holidaze/precache-manifest.b354c57eba9ad0cd9608d2d97793c735.js"
+  "/precache-manifest.5789281b4296f39a15d10fb41fea0f2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/holidaze/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
